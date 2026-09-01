42b4eb117e1b523e015b7ec5fb82d1c503a21ed6
Mohammad Eslami <eslami.mhmd@gmail.com>
2026-09-01 22:43:08 +0330

    chore: configure dependency injection and application bootstrap

---
+++ src/SupportCenter.Api/Program.cs
@@ -0,0 +1,31 @@
+using SupportCenter.Application;
+using SupportCenter.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
